28f3557c5c426c819acfbb1dd8278b4425c7a0e5
csliva <colt.sliva@gmail.com>
2019-09-27 11:35:16 -0400

    looping and bar states

--- components/StepSequencer.vue
+++ components/StepSequencer.vue
@@ -1,8 +1,13 @@
 <template>
-  <div class="seq">
-    <div v-for="k in 12" class="seq__keyrow">
-      <node v-for="i in 8" />
+  <div>
+    <div class="seq">
+      <div v-for="k in 12" class="seq__keyrow">
+        <node v-for="i in 8" v-bind:row="i" v-bind:step="step" />
+      </div>
     </div>
+    <button class="play" @click="play">
+      Play
+    </button>
   </div>
 </template>
 
@@ -13,7 +18,19 @@
       Node
     },
     data: function(){
-      return {}
+      return {
+        step: 0
+      }
+    },
+    methods: {
+      play: function(){
+        this.iteratePlay()
+      },
+      iteratePlay: function(){
+        if (this.step < 8){ this.step = this.step + 1 }
+        else { this.step = 1 }
+        console.log(this.step)
+      }
     }
   }
 </script>
--- components/StepSequencer/Node.vue
+++ components/StepSequencer/Node.vue
@@ -1,24 +1,42 @@
 <template>
   <span
-  class="seq__bar"
-  v-on:click="activate"
-  v-bind:class="{ active: active }"
+  class="seq__bar noselect"
+  v-on:mousedown="down"
+  v-on:mouseup="up"
+  v-on:mouseover="hover"
+  v-bind:class="{ active: active, highlight: this.step === this.row }"
   >
-    BAR
+    {{ this.row }}
   </span>
 </template>
 
 <script>
 export default {
+  props: ['row', 'step'],
   data: function() {
     return {
       octave: '4',
       active: false,
+      row: this.row,
+      step: this.step,
+      notes: [
+        {id: "", }
+      ]
     }
   },
   methods: {
-    activate: function(){
+    down: function(){
+      //toggle active state to opposite
       this.active = !this.active
+      //set global var to toggle state
+      window.active = this.active
+      window.mickey = true
+    },
+    up: function(){ window.mickey = false },
+    hover: function(){
+      if (window.mickey) {
+        this.active = window.active
+      }
     }
   }
 }
